b4462fbde44f37004daaaa002fcb434720040b30
Leosandy <leosandy595@gmail.com>
2021-11-27 14:57:50 +0700

    pts

---
+++ app/src/main/java/com/example/ptswahyu33/DataModel.java
@@ -0,0 +1,46 @@
+package com.example.ptswahyu33;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class DataModel extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+    private String image;
+    private String nama;
+    private String deskripsi;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
+
